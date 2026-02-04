b94d67b0fa8de21ecc66ef102589732f562534f6
mertunctuncer <mertunctuncer@gmail.com>
2026-02-05 00:08:15 +0300

    build: match kotlin plugin and kotlin stdlib versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.ow2.asm:asm:9.9.1")
 
